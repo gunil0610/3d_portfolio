7c489f6c3ed18cdd577ba38900aebf5baf8b926f
gunil0610 <gunil.na1@gmail.com>
2023-03-16 11:52:40 +0900

    feat: basic projec setup, download assets, css, made structures of project

---
+++ src/utils/motion.ts
@@ -0,0 +1,110 @@
+// TODO: configure type later again
+type HiddenDirection = string;
+type TransitionType = string;
+type TransitionDelay = number;
+type TransitionDuration = number;
+
+export const textVariant = (delay: TransitionDelay) => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const fadeIn = (
+  direction: HiddenDirection,
+  type: TransitionType,
+  delay: TransitionDelay,
+  duration: TransitionDuration
+) => {
+  return {
+    hidden: {
+      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const zoomIn = (
+  delay: TransitionDelay,
+  duration: TransitionDuration
+) => {
+  return {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const slideIn = (
+  direction: HiddenDirection,
+  type: TransitionType,
+  delay: TransitionDelay,
+  duration: TransitionDuration
+) => {
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const staggerContainer = (
+  staggerChildren: string,
+  delayChildren: number
+) => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: staggerChildren,
+        delayChildren: delayChildren || 0,
+      },
+    },
+  };
+};
