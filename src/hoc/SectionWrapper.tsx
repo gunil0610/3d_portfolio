1714df1db88d011ebdbf504aff273a44fe0b92a9
gunil0610 <gunil.na1@gmail.com>
2023-03-20 15:44:39 +0900

    refactor: refactor SectionWrapper to arrow function

--- src/hoc/SectionWrapper.tsx
+++ src/hoc/SectionWrapper.tsx
@@ -4,24 +4,22 @@ import { styles } from "../styles";
 import { staggerContainer } from "@src/utils/motion";
 import React, { PropsWithChildren } from "react";
 
-function SectionWrapper<T extends PropsWithChildren>(
-  Component: React.FC<T>,
-  idName: string
-) {
-  return (hocProps: T) => (
-    <motion.section
-      variants={staggerContainer()}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true, amount: 0.25 }}
-      className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-    >
-      <span className="hash-span" id={idName}>
-        &nbsp;
-      </span>
-      <Component {...hocProps} />
-    </motion.section>
-  );
-}
+const SectionWrapper =
+  <T extends PropsWithChildren>(Component: React.FC<T>, idName: string) =>
+  (hocProps: T) =>
+    (
+      <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className="hash-span" id={idName}>
+          &nbsp;
+        </span>
+        <Component {...hocProps} />
+      </motion.section>
+    );
 
 export default SectionWrapper;
